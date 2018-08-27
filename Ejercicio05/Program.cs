77dc76198891745cccfc83782aaf2ee27f25ec7a
creavenw22 <francisco.bustamantew22@gmail.com>
2018-08-27 15:22:29 -0300

    Ejercicio_06 termina2

---
+++ Ejercicio05/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio05
+{
+    class EJercicio_05
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 05";
+            int num;
+            int nGroup1 = 0;
+            int nGroup2 = 0;
+            int cont = 1;
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Un centro numerico es un\nnumero que separa una\nlista de numeros enteros\nen dos grupos numericos\ncuyas sumas son iguales.");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Ingrese un numero para mostrar todos los centros numericos hasta ese numero: ");
+
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            for (int i = 2; i <= num; i++)
+            {
+                for(int g1=0;g1<i;g1++)
+                {
+                    nGroup1 = nGroup1 + g1;
+                }
+
+                for(int g2=i+1;nGroup2<nGroup1;g2++)
+                {
+                    nGroup2 = nGroup2 + g2;
+                }
+
+                if (nGroup1 == nGroup2)
+                {
+                    Console.WriteLine("El centro numerico numero {0} es: {1}",cont,i);
+                    cont++;
+                }
+                nGroup1 = nGroup2 = 0;  
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio06/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio06
+{
+    class Ejercicio_06
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 06";
+            int year=-1;
+
+            
+            while (year != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Ingrese un año, el programa le informara si es un año bisiesto o no.");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nIngrese 0 para salir.");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Ingrese el año: ");
+                
+                year = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                if ((year % 4 == 0) && (year !=0 ))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nEl año {0} es bisiesto.\n", year);
+                }
+
+                else if (year == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nSaliendo del programa.\n");
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nEl año {0} no es bisisesto.\n", year);
+                }
+
+                //Console.ReadLine();
+                //Console.Clear();
+
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
