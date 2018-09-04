c0ed6e2f32abf10021218c29024ab8bf8365bfef
Creavenw22 <francisco.bustamantew22@gmail.com>
2018-09-03 23:40:28 -0300

    Exe_18 in progress

---
+++ Ejercicio17/Boligrafo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    class Boligrafo
+    {
+        #region Atributos
+        public const short cantidadTintaMax = 100;
+        private ConsoleColor color;
+        private short tinta;
+        #endregion
+
+        #region Metodos
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            short test = (short)(this.GetTinta() + tinta);
+            if ((test >= 0) && (test <= cantidadTintaMax))
+            {
+                this.tinta = (short)(this.tinta + tinta);
+                Console.WriteLine("\nEl nivel actual de tinta es: {0}", this.tinta);
+            }
+            else if (test < 0)
+            {
+                Console.WriteLine("\nNo se pudo hacer el gasto porque alcanza la tinta");
+            }
+            else if (test > cantidadTintaMax)
+            {
+                Console.WriteLine("\nNo se pudo hacer la recarga porque supera la capacidad maxima");
+            }
+        }
+
+        public void Recargar()
+        {
+            short missingTint=(short)(cantidadTintaMax-this.tinta);
+
+            SetTinta(missingTint);
+        }
+
+        public bool Pintar(short tinta,out string dibujo)
+        {
+            bool ret=false;
+            dibujo = "";
+
+            return ret;
+        }
+
+
+        #endregion
+
+        #region Contructor
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Ejercicio17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Objects;
+
+namespace Ejercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo boliAzul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo boliRojo = new Boligrafo(ConsoleColor.Red, 50);
+
+
+        }
+    }
+}
